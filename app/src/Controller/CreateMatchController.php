f0664000b70c45ba71bcb8eaf1da92e55aa85194
Zeus <zeus.albu@gmail.com>
2022-09-07 17:25:35 +0300

    create match service provider + return password in create match view

--- app/src/Controller/CreateMatchController.php
+++ app/src/Controller/CreateMatchController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Service\CreateMatchServiceProvider;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,13 +10,13 @@ use Symfony\Component\Routing\Annotation\Route;
 class CreateMatchController extends AbstractController
 {
     #[Route('/createMatch', name: 'create_match')]
-    public function index(): Response
+    public function index(CreateMatchServiceProvider $service): Response
     {
         if (!$this->getUser()) {
             return $this->redirectToRoute('app_login');
         }
 
-        return $this->render('/createMatch/createMatch.html.twig');
+        return $this->render('/createMatch/createMatch.html.twig', ['password' => $service->matchPassword()]);
     }
 
 }
---
+++ app/src/Service/CreateMatchServiceProvider.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Service;
+
+use Symfony\Component\Security\Core\Security;
+
+class CreateMatchServiceProvider
+{
+    public function __construct(protected Security $security)
+    {
+    }
+
+    public function matchPassword(): int
+    {
+        try {
+            return random_int(100, 1000) * 10 + $this->security->getUser()->getId();
+        } catch (\Exception $e) {
+            return $this->security->getUser()->getId() + 123123;
+        }
+    }
+}
