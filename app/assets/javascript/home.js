23e4516f9e8ce152f523aa9b14989ed0cab36726
andreinicolescu <nicolescu.andrei_18@yahoo.ro>
2022-09-07 17:30:40 +0300

    ticket_012->send-request-from-modal-to-create_match-controller

--- app/assets/javascript/home.js
+++ app/assets/javascript/home.js
@@ -1,3 +1,7 @@
+import axios from "axios";
+
+import {customAlertError} from "./customAlerts";
+
 let createMatchModal = document.getElementById('modal-create-match')
 
 let shipsButtons = document.getElementById('form-section-ships-create-match').children
@@ -21,6 +25,7 @@ for (let shipsButton of shipsButtons) {
         shipsButton.classList.add('selected-ship')
     })
 }
+
 for (let shotsButton of shotsButtons) {
     shotsButton.addEventListener('click', ev => {
         let lastSelected = document.getElementsByClassName('selected-shot')
@@ -33,7 +38,27 @@ for (let shotsButton of shotsButtons) {
 
 
 submitModalButton.addEventListener("click", ev => {
-    // let numberOfBoats =
+    let selectedShipsButton = document.getElementsByClassName('selected-ship')
+    let selectedShotsButton = document.getElementsByClassName('selected-shot')
+
+    if (selectedShipsButton.item(0) !== null && selectedShotsButton.item(0) !== null) {
+        let numberOfShots = selectedShotsButton.item(0).getAttribute('data-value')
+        let numberOfShips = selectedShipsButton.item(0).getAttribute('data-value')
+
+        const dataToSend = new FormData()
+        dataToSend.set('ships', numberOfShips)
+        dataToSend.set('shots', numberOfShots)
+
+        axios.post(
+            '/createMatch',
+            dataToSend,
+        ).then(function (response) {
+            console.log('sent')
+        });
+
+    } else {
+        customAlertError('Please select all the boxes!')
+    }
 })
 
 let closeModalButton = document.getElementById('modal-close-button')
