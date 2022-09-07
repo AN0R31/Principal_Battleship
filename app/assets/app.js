7deb2bac15d0775a4fc459c9fee466d5916b0425
andreinicolescu <nicolescu.andrei_18@yahoo.ro>
2022-09-07 14:23:15 +0300

    ticket_002->create-button-in-home-page-to-redirect-to-create_match

--- app/assets/app.js
+++ app/assets/app.js
@@ -7,6 +7,9 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
+import './styles/home.css';
 
 // start the Stimulus application
 import './bootstrap';
+
+import './javascript/home';
