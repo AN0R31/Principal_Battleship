156372abc0068d3bc8fb0d132afb75e845c7dac2
andreinicolescu <nicolescu.andrei_18@yahoo.ro>
2022-09-22 20:11:22 +0300

    ticket_113->add-watch-ongoing-matches-functionality-to-home-page

--- app/src/Controller/HomeController.php
+++ app/src/Controller/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Battle;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,11 +11,25 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(EntityManagerInterface $entityManager): Response
     {
         if (!$this->getUser()) {
             return $this->redirectToRoute('app_login');
         }
-        return $this->render('home/home.html.twig');
+
+        $ongoingBattles = $entityManager->getRepository(Battle::class)->findBy(['winner_id' => null], array('id' => 'DESC'), 100);
+
+        $key = 0;
+        foreach ($ongoingBattles as $ongoingBattle) {
+            if ($ongoingBattle->getUser2() === null) {
+                array_splice($ongoingBattles, $key, 1);
+                $key--;
+            }
+            $key++;
+        }
+
+        array_splice($ongoingBattles, 4, sizeof($ongoingBattles));
+
+        return $this->render('home/home.html.twig', ['ongoingBattles' => $ongoingBattles]);
     }
 }
