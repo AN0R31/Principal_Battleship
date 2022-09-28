a3bc3be71936c7a13c0657c4e51249c1b3d742f1
andreinicolescu <nicolescu.andrei_18@yahoo.ro>
2022-09-28 18:47:46 +0300

    ticket_137->create-user-profile-page

--- app/src/Controller/ProfileController.php
+++ app/src/Controller/ProfileController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Battle;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,6 +13,40 @@ class ProfileController extends AbstractController
     #[Route('/profile', name: 'profile')]
     public function index(EntityManagerInterface $entityManager): Response
     {
-        return $this->render('profile/profile.html.twig', []);
+        $thisUser = $this->getUser();
+
+        if (!$thisUser) {
+            return $this->redirectToRoute('app_login');
+        }
+
+        $lastMatches = $entityManager->getRepository(Battle::class)->findBy(['user1_id' => $thisUser->getId()]);
+
+        $lastMatches2 = $entityManager->getRepository(Battle::class)->findBy(['user2_id' => $thisUser->getId()]);
+
+        foreach ($lastMatches2 as $match) {
+            $lastMatches[] = $match;
+        }
+
+        $key = 0;
+        foreach ($lastMatches as $lastMatch) {
+            $winner = $lastMatch->getWinner();
+
+            if ($winner === null) {
+                array_splice($lastMatches, $key, 1);
+                $key--;
+            }
+            $key++;
+        }
+
+        $id = array();
+        foreach ($lastMatches as $key => $lastMatch)
+        {
+            $id[$key] = $lastMatch->getId();
+        }
+        array_multisort($id, SORT_DESC, $lastMatches);
+
+        array_splice($lastMatches, 7, sizeof($lastMatches));
+
+        return $this->render('profile/profile.html.twig', ['lastMatches' => $lastMatches]);
     }
 }
